42a448112894c4ba27efde1887b008bfe6e63e5a
Reinaldo <reinaldodosanpb@gmail.com>
2026-07-12 12:26:07 +0100

    feat(chapter-01): add Exercise 03 - Using print and println

---
+++ chapter-01/src/Chap01Exercise03.java
@@ -0,0 +1,17 @@
+/*
+ * Chapter 1 - Exercise 03
+ * Book: Think Java (2nd Edition)
+ * Objective:
+ * Practice the use of print() and println()
+ * by displaying a sentence on a single line.
+ *
+ * Concepts Practiced:
+ * - print()
+ * - println()
+ */
+public class Chap01Exercise03 {
+    public static void main(String[] args) {
+     System.out.print("Welcome to ");
+     System.out.println("Java Programming!");   
+    }
+}
