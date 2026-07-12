5503a4e690b4961fd8a66c2f0538dc121edfd2ea
Reinaldo <reinaldodosanpb@gmail.com>
2026-07-12 12:31:28 +0100

    feat(chapter-01): add Exercise 04 - Practice the use of escape sequences (\n and \t)

---
+++ chapter-01/src/Chap01Exercise04.java
@@ -0,0 +1,17 @@
+/*
+ * Chapter 1 - Exercise 04
+ * Book: Think Java (2nd Edition)
+ *
+ * Objective:
+ * Practice the use of escape sequences (\n and \t)
+ * to format text output.
+ *
+ * Author: Reinaldo Santos
+ */
+public class Chap01Exercise04 {
+    public static void main(String[] args) {
+        System.out.println("Student Information\n\tName: Reinaldo\n\tLanguage: Java\n\tBook: Think Java");
+        System.out.print("\n\n");
+        System.out.print("Let's learn Java!");        
+    }
+}
