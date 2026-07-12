fc45552d30675678537ca44a38b5bc20fe9dee73
Reinaldo <reinaldodosanpb@gmail.com>
2026-07-12 12:34:22 +0100

    feat(chapter-01): add Exercise 05 - Practice using escaped quotation marks.

---
+++ chapter-01/src/Chap01Exercise05.java
@@ -0,0 +1,19 @@
+/*
+ * Chapter 1 - Exercise 05
+ * Book: Think Java (2nd Edition)
+ *
+ * Objective:
+ * Practice using escaped quotation marks (\" ) inside strings.
+ *
+ * Requirements:
+ * - Display Albert Einstein's quote.
+ * - Use escaped quotation marks (\") correctly.
+ *
+ * Author: Reinaldo Santos
+  */
+public class Chap01Exercise05 {
+    public static void main(String[] args) {
+     System.out.println("Albert Einstein said:");
+     System.out.println("\"Life is like riding a bicycle. To keep your balance, you must keep moving.\"");    
+    }
+}
