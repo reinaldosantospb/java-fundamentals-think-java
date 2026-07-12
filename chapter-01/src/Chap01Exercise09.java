09a2ee5f046bcc032f6525ec932360bd825f2484
Reinaldo <reinaldodosanpb@gmail.com>
2026-07-12 12:46:18 +0100

    feat(chapter-01): add Exercise 09 - Review the basic structure of a Java program by identifying the purpose of the class.

---
+++ chapter-01/src/Chap01Exercise09.java
@@ -0,0 +1,18 @@
+/*
+ * Chapter 1 - Exercise 09
+ * Book: Think Java (2nd Edition)
+ *
+ * Objective:
+ * Review the basic structure of a Java program
+ * by identifying the purpose of the class,
+ * the main() method, and the print statements
+ * using comments.
+ */
+public class Chap01Exercise09 {
+    // The main method is the entry point of the program.
+    // Execution starts here.
+    public static void main(String[] args) {
+    // Prints a message to the console.
+    System.out.println("Understanding Java Structure");   
+    }
+}
