b85836a943db168c9f3cf9e79f584628b3238cf9
Reinaldo <reinaldodosanpb@gmail.com>
2026-08-17 05:16:26 +0100

    fix(chapter-03): close Scanner resource in Exercise 06

--- chapter-03/src/Chap03Exercise06.java
+++ chapter-03/src/Chap03Exercise06.java
@@ -1,3 +1,19 @@
+/*
+ * Chapter 3 - Exercise 6
+ *
+ * This program is a Guess My Number game.
+ *
+ * It generates a random number between 1 and 100 using the Random class,
+ * prompts the user to enter a guess, and reads the input using Scanner.
+ * The program then calculates the absolute difference between the user's
+ * guess and the generated number and displays the results.
+ *
+ * Example:
+ * Guess a number between 1 and 100: 45
+ * Your guess is: 45
+ * The random number is: 14
+ * The difference between your guess and the number is: 31
+ */
 import java.util.Scanner;
 import java.util.Random;
 public class Chap03Exercise06 {
@@ -12,7 +28,7 @@ public class Chap03Exercise06 {
     int difference = Math.abs(guess - number);
     System.out.println("The random number is: " + number);
     System.out.println("The difference between your guess and the number is: " + difference);
-          
+    in.close();         
 
    }     
 }
